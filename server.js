8b4490c09e396fae02bb363f8041927544043655
zhatfiel <zhatfiel@uncc.edu>
2020-12-18 19:13:15 -0800

    began implementing auth0 and adding it to the login route

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ app.use(cors());
 
 app.use('/', express.static('public'));
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.get('/login', function(req, res) {
+    var auth0 = new auth0.WebAuth({
+        domain: 'https://zhatfiel-final-project.herokuapp.com/',
+        clientID: 'WcMiRD38uQySAvwyu0pD05OyyY92RJ31'
+      });
 });
 
 //populate pie chart
