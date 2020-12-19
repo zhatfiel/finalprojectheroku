5f2f94112ce6931df8474249fe861c22f24e7128
zhatfiel <zhatfiel@uncc.edu>
2020-12-18 19:43:55 -0800

    trying a different placement for the cors allow code snippet

--- server.js
+++ server.js
@@ -14,12 +14,6 @@ const url = process.env.MONGODB_URI;
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://zhatfiel-final-project.herokuapp.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 app.use('/', express.static('public'));
 
 //auth0 config
@@ -38,8 +32,11 @@ const config = {
 app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://broad-shape-4582.us.auth0.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  next();
 });
 
 //populate pie chart
