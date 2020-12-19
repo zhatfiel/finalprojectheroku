6cd7d565d1dd144be08af3b8870d609156974faf
zhatfiel <zhatfiel@uncc.edu>
2020-12-18 19:38:00 -0800

    clearing up cross site access policy issues

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const url = process.env.MONGODB_URI;
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://broad-shape-4582.us.auth0.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use('/', express.static('public'));
 
 //auth0 config
